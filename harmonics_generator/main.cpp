80d44c7992e7b14697bcb651449004c533d57dd6
Jack <jack.morgan.edwards@gmail.com>
2022-10-01 18:21:06 -0400

    Initial Commit

---
+++ harmonics_generator/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  harmonics_generator
+//
+//  Created by Jack Edwards on 10/1/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
