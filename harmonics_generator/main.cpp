57ec1ba8424ca8661251c2ad816eeab2d0c4dddf
Jack <jack.morgan.edwards@gmail.com>
2022-10-01 18:38:29 -0400

    initial commit

--- harmonics_generator/main.cpp
+++ harmonics_generator/main.cpp
@@ -1,14 +1,21 @@
-//
-//  main.cpp
-//  harmonics_generator
-//
-//  Created by Jack Edwards on 10/1/22.
-//
-
 #include <iostream>
+#include <vector>
+#include <stdlib.h>
 
 int main(int argc, const char * argv[]) {
-  // insert code here...
-  std::cout << "Hello, World!\n";
+  
+  const double HZ_limit = 20000.0;
+  
+  std::vector<double> harmonics;
+  double fund {atof(argv[1])};
+  int partial {1};
+  
+  while (fund * partial <= HZ_limit) {
+    harmonics.push_back(fund * partial);
+    partial += 1;
+  }
+  for (double i: harmonics)
+      std::cout << i << ' ';
+  
   return 0;
 }
